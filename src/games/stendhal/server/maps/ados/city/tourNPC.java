0c8623ced6f6524fb48baace01019e9555b1905e
j41395dr <daniel.ross-3@student.manchester.ac.uk>
2021-12-02 15:41:41 +0000

    Tour feature partially implemented
    Player teleported to ados sewing and dialogue given

--- src/games/stendhal/server/maps/ados/city/tourNPC.java
+++ src/games/stendhal/server/maps/ados/city/tourNPC.java
@@ -11,16 +11,22 @@
  ***************************************************************************/
 package games.stendhal.server.maps.ados.city;
 
-//import java.util.Arrays;
 import java.util.Map;
+
+import games.stendhal.common.parser.Sentence;
 
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
-//import games.stendhal.server.core.events.UseListener;
-//import games.stendhal.server.entity.CollisionAction;
+
+import games.stendhal.server.entity.Entity;
+
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-//import games.stendhal.server.entity.RPEntity;
-//import games.stendhal.server.entity.npc.SilentNPC;
+
+import games.stendhal.server.entity.player.Player;
 
 /**
  * A mysterious mage that gives tours
@@ -28,7 +34,9 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
  * @author j41395dr
  */
 public class tourNPC implements ZoneConfigurator {
-
+	
+	Integer pointer = 0;
+	
 	@Override
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
 		buildNPC(zone);
@@ -39,27 +47,45 @@ public class tourNPC implements ZoneConfigurator {
 		final SpeakerNPC npc = new SpeakerNPC("Mysterious Mage") {
 			@Override
             protected void createPath() {
-//                List<Node> nodes=new LinkedList<Node>();
-//                nodes.add(new Path.Node(9,5));
-//                nodes.add(new Path.Node(14,5));
-//                setPath(nodes,true);
             	setPath(null);
             }
+			
+			 final ChatCondition startofTour = new ChatCondition() {
+					@Override
+					public boolean fire(final Player player, final Sentence sentence, final Entity npc) {
+						if (pointer == 0) {
+							return true;
+						} else {
+							return false;
+						}
+					}
+				};
             
             @Override
             protected void createDialog() {
-                // Lets the NPC reply with "Hallo" when a player greets him. But we could have set a custom greeting inside the ()
-                addGreeting("Hello there traveller, I am offering #tour of the local area if you are interested?");
-                // Lets the NPC reply when a player says "job"
+                addGreeting("Hello there traveller, I am offering a #tour of the local area if you are interested?");
                 addJob("I'm a simple man making his way through the galaxy. Like my father before me. ");
-                // Lets the NPC reply when a player asks for help
                 addHelp("I can help you with information about the local area");
-                // respond about a special trigger word
-                addReply("tour","Welp, let's get started then!.");
-                // use standard goodbye, but you can also set one inside the ()
+                add(ConversationStates.ATTENDING, "tour", null, ConversationStates.QUESTION_1, null,
+				        new ChatAction() {
+					        @Override
+							public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {		        	
+					        		startTour(player, npc);
+					        	
+					        }
+				        });
+                add(ConversationStates.QUESTION_1, "continue", null, ConversationStates.QUESTION_2, null,
+                		new ChatAction() {
+                			@Override
+                			public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {		        	
+                					stage2(player, npc);
+                			}
+                });
+             
                 addGoodbye("Goodbye my friend!");
             }
         };
+        
 		
 		npc.setPosition(34, 80);
 		npc.setDescription("You see a mage who seems very knowledgable");
@@ -67,5 +93,28 @@ public class tourNPC implements ZoneConfigurator {
 		npc.initHP(100);
 		zone.add(npc);
 	}
-
+	
+	private void startTour(Player player, EventRaiser npc) {
+		
+		Entity entity = npc.getEntity();
+		
+		player.setInvisible(true);
+		
+		entity.setPosition(45, 60);
+		player.teleport("0_ados_city", 45,59,null,null);
+		
+		npc.say("To the left is Ados' sewing room, within is Ida who is a seamstress and she makes sails!");
+		npc.say("To the right is a house with a funny number hehe, I do know how to make the kids laugh");
+		npc.say("Tell me when you want to #continue");
+		
+		
+	}
+	
+	private void stage2(Player player, EventRaiser npc) {
+		Entity entity = npc.getEntity();
+		
+		entity.setPosition(40, 60);
+		player.teleport("0_ados_city", 40,59,null,null);
+	}
+	
 }
