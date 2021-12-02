35f45bc219812ae97d1632fef4fa715017b08359
j41395dr <daniel.ross-3@student.manchester.ac.uk>
2021-12-02 11:44:23 +0000

    Updated test for NPC

--- tests/games/stendhal/server/maps/ados/city/tourNPCTest.java
+++ tests/games/stendhal/server/maps/ados/city/tourNPCTest.java
@@ -1,31 +1,50 @@
 package games.stendhal.server.maps.ados.city;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.player.Player;
+import utilities.QuestHelper;
 import utilities.ZonePlayerAndNPCTestImpl;
 
 public class tourNPCTest extends ZonePlayerAndNPCTestImpl {
 
 	private Player player;
 	private SpeakerNPC npc;
+	private Engine engine;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		setupZone("0_ados_city");
+	}
+	
+	public tourNPCTest() {
+		setNpcNames("Mysterious Mage");
+		setZoneForPlayer("0_ados_city");
+		addZoneConfigurator(new tourNPC(), "0_ados_city");
+	}
 
 	@Override
 	@Before
 	public void setUp() throws Exception {
-		player = createPlayer("abcd");
+		super.setUp();
+		player = createPlayer("player");
+		npc = SingletonRepository.getNPCList().get("Mysterious Mage");
+		engine = npc.getEngine();
 	}
 
 	@Test
 	public void NPCExistsTest() {
-//		Player player = PlayerTestHelper.createPlayer("abcd");
-		npc = SingletonRepository.getNPCList().get("Mysterious Mage");
-		assertNotNull(npc);
+		engine.step(player, ConversationPhrases.GREETING_MESSAGES.get(0));
+		assertTrue(npc.isTalking());
 	}
 
 }
